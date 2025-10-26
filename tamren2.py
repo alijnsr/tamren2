5d8e8b2579b5635ea1cffae98147348dc6aa499d
alijnsr <alijnsr2004@gmail.com>
2025-10-26 09:51:19 -0700

    Update tamren2.py

--- tamren2.py
+++ tamren2.py
@@ -10,3 +10,26 @@ print("روز" ,mohasenroz)
 print("ساعت" ,sensaat)
 print("دقیقه" ,sendaghi)
 print("ثانیه" ,sensani)
+#تمرین دوم 
+
+#shomare = int(input("دورقمی "))
+#خیلی فرق ها دارن / با// تکی معمولی هست 2 تایی صحیح
+#ten = shomare // 10
+#%باقیمانده تقسیم 
+#amaleat = shomare % 10
+#shomaremakos = amaleat * 10 + ten
+#print("عدد معکوس :", shomaremakos)
+
+#تمرین سوم
+
+
+#shomare = int(input("یک عدد دورقمی "))
+#ten = shomare // 10
+#amaleat = shomare % 10
+# one =1
+#ten=10
+#**یعنی توان یکی باشد میشود ضرب
+#tentaone = ten ** amaleat   # رقم اول به توان رقم دوم
+#onetaten = amaleat ** ten   # رقم دوم به توان رقم اول
+#print("رقم اول به توان رقم دوم:", tentaone)
+#print("رقم دوم به توان رقم اول:", onetaten)
