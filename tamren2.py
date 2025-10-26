0d75222d30010664e1b538d74a3b4b0e470c56b5
alijnsr <alijnsr2004@gmail.com>
2025-10-26 10:11:49 -0700

    Update tamren2.py

--- tamren2.py
+++ tamren2.py
@@ -1,15 +1,15 @@
 #تمرین اول تبدیل سن به ساعت  دقیقه ثانیه
 
-#sensal = int(input(" سن خود را وارد کنید  "))
-#rozsal = 365
-#mohasenroz = sensal * rozsal
-#sensaat = mohasenroz * 24
-#sendaghi = sensaat * 60
-#sensani = sendaghi * 60
-#print("روز" ,mohasenroz)
-#print("ساعت" ,sensaat)
-#print("دقیقه" ,sendaghi)
-#print("ثانیه" ,sensani)
+sensal = int(input(" سن خود را وارد کنید  "))
+rozsal = 365
+mohasenroz = sensal * rozsal
+sensaat = mohasenroz * 24
+sendaghi = sensaat * 60
+sensani = sendaghi * 60
+print("روز" ,mohasenroz)
+print("ساعت" ,sensaat)
+print("دقیقه" ,sendaghi)
+print("ثانیه" ,sensani)
 #تمرین دوم 
 
 #shomare = int(input("دورقمی "))
